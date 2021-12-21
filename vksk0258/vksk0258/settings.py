58ec186f76d91666390431e9a361ebfab27ed41a
DESKTOP-A18V5M1\vksk0 <giltaek.yu@apexsoft.co.kr>
2021-12-21 17:35:48 +0900

    1221

--- vksk0258/vksk0258/settings.py
+++ vksk0258/vksk0258/settings.py
@@ -131,7 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # 호스트이름 작성 가능
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'vksk0258', 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
